ab291d03b2ec58115c872d9bfa1803e0e6de246c
andres alcocer <andresalcocer7@yahoo.com>
2021-09-22 14:15:17 -0400

    installed navigation packages

---
+++ App.js
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {NavigationContainer} from '@react-navigation/native';
+
+export default function App() {
+  return <NavigationContainer />;
+}
