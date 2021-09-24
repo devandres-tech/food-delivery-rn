7f1fd3959523ccac03c3b0699611bdbb7626de27
andres alcocer <andresalcocer7@yahoo.com>
2021-09-24 13:53:19 -0400

    successfuly ran app on android device

--- android/app/src/main/java/com/rncliappdemo/MainActivity.java
+++ android/app/src/main/java/com/rncliappdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rncliappdemo;
 
-import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;;
 
 public class MainActivity extends ReactActivity {
 
@@ -15,7 +17,12 @@ public class MainActivity extends ReactActivity {
   }
 
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
