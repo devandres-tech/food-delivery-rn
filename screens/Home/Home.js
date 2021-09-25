68aaef02c11ae0b1ac0bef7051982e459198220b
andres alcocer <andresalcocer7@yahoo.com>
2021-09-25 13:17:57 -0400

    created search content in Home screen component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,16 +1,99 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  FlatList,
+} from 'react-native'
+
+import { FONTS, SIZES, COLORS, icons, dummyData } from '../../constants'
 
 const Home = () => {
+  const [selectedCategoryId, setSelectedCategoryId] = useState(1)
+  const [selectedMenuType, setSelectedMenuType] = useState(1)
+  const [menuList, setMenuList] = useState([])
+
+  useEffect(() => {
+    handleChangeCategory(selectedCategoryId, selectedMenuType)
+  }, [])
+
+  const handleChangeCategory = (categoryId, menuTypeId) => {
+    let selectedMenu = dummyData.menu.find(
+      (menuItem) => menuItem.id === menuTypeId
+    )
+    console.log('selected menu', selectedMenu)
+    setMenuList(
+      selectedMenu.list.filter((item) => item.categories.includes(categoryId))
+    )
+  }
+
+  const renderSearch = () => {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          height: 40,
+          alignItems: 'center',
+          marginHorizontal: SIZES.padding,
+          marginVertical: SIZES.base,
+          paddingHorizontal: SIZES.radius,
+          borderRadius: SIZES.radius,
+          backgroundColor: COLORS.lightGray2,
+        }}
+      >
+        <Image
+          source={icons.search}
+          style={{ height: 20, width: 20, tintColor: COLORS.black }}
+        />
+        <TextInput
+          style={{ flex: 1, marginLeft: SIZES.radius, ...FONTS.body3 }}
+        />
+        <TouchableOpacity>
+          <Image
+            source={icons.filter}
+            style={{ height: 20, width: 20, tintColor: COLORS.black }}
+          />
+        </TouchableOpacity>
+      </View>
+    )
+  }
+
   return (
     <View
       style={{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
       }}
     >
-      <Text>Home</Text>
+      {/* search */}
+      {renderSearch()}
+
+      {/* list  */}
+      <FlatList
+        data={menuList}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => `${item.id}`}
+        renderItem={({ item, index }) => {
+          return (
+            <HorizontalFoodCard
+              containerStyle={{
+                height: 130,
+                alignItems: 'center',
+                marginHorizontal: SIZES.padding,
+                marginBottom: SIZES.radius,
+              }}
+              imageStyle={{
+                marginTop: 20,
+                height: 110,
+                width: 110,
+              }}
+              item={item}
+              onPress={() => console.log('horizontal food card')}
+            />
+          )
+        }}
+      />
     </View>
   )
 }
