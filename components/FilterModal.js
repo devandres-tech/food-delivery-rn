42231dfecf59296ecb16844707492815c1288641
andres alcocer <andresalcocer7@yahoo.com>
2021-09-27 14:16:48 -0400

    created modal header content

--- components/FilterModal.js
+++ components/FilterModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import {
   View,
   Text,
@@ -7,14 +7,34 @@ import {
   TouchableWithoutFeedback,
   Modal,
 } from 'react-native'
+
 import { COLORS, FONTS, SIZES, constants, icons } from '../constants'
+import IconButton from './IconButton'
 
 const FilterModal = ({ isVisible, onClose }) => {
   const [showFilterModal, setShowFilterModal] = useState(isVisible)
+  const modalAnimatedValue = useRef(new Animated.Value(0)).current
 
   useEffect(() => {
-    if (!showFilterModal) onClose()
+    if (showFilterModal) {
+      Animated.timing(modalAnimatedValue, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: false,
+      }).start()
+    } else {
+      Animated.timing(modalAnimatedValue, {
+        toValue: 0,
+        duration: 500,
+        useNativeDriver: false,
+      }).start(() => onClose())
+    }
   }, [showFilterModal])
+
+  const modalY = modalAnimatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [SIZES.height, SIZES.height - 680],
+  })
 
   return (
     <Modal animationType='fade' transparent={true} visible={isVisible}>
@@ -30,6 +50,36 @@ const FilterModal = ({ isVisible, onClose }) => {
             }}
           ></View>
         </TouchableWithoutFeedback>
+        <Animated.View
+          style={{
+            position: 'absolute',
+            left: 0,
+            top: modalY,
+            width: '100%',
+            height: '100%',
+            padding: SIZES.padding,
+            borderTopRightRadius: SIZES.padding,
+            borderTopLeftRadius: SIZES.padding,
+            backgroundColor: COLORS.white,
+          }}
+        >
+          {/* header  */}
+          <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+            <Text style={{ flex: 1, ...FONTS.h3, fontSize: 18 }}>
+              Filter Your Search
+            </Text>
+            <IconButton
+              containerStyle={{
+                borderWith: 2,
+                borderRadius: 10,
+                borderColor: COLORS.gray2,
+              }}
+              icon={icons.cross}
+              iconStyle={{ tintColor: COLORS.gray2 }}
+              onPress={() => setShowFilterModal(false)}
+            />
+          </View>
+        </Animated.View>
       </View>
     </Modal>
   )
--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Header from './Header'
 import HorizontalFoodCard from './HorizontalFoodCard'
 import VerticalFoodCard from './VerticalFoodCard'
 import FilterModal from './FilterModal'
+import IconButton from './IconButton'
 
-export { Header, FilterModal, HorizontalFoodCard, VerticalFoodCard }
+export { Header, FilterModal, HorizontalFoodCard, VerticalFoodCard, IconButton }
