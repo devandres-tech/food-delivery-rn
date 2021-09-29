b2228506a61e020b217d65d2398965735f78ca02
andres alcocer <andresalcocer7@yahoo.com>
2021-09-29 10:08:12 -0400

    set up andriod splash screen

--- android/app/src/main/java/com/rncliappdemo/MainActivity.java
+++ android/app/src/main/java/com/rncliappdemo/MainActivity.java
@@ -1,9 +1,11 @@
 package com.rncliappdemo;
 
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;;
+import org.devio.rn.splashscreen.SplashScreen; // here
 
 public class MainActivity extends ReactActivity {
 
@@ -24,5 +26,11 @@ public class MainActivity extends ReactActivity {
        return new RNGestureHandlerEnabledRootView(MainActivity.this);
       }
     };
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
   }
 }
