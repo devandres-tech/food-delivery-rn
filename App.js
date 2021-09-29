23e763646e62e0064664b0956bf86cfd1d12da14
andres alcocer <andresalcocer7@yahoo.com>
2021-09-29 10:21:34 -0400

    set up auth screens

--- App.js
+++ App.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import SplashScreen from 'react-native-splash-screen'
 
 import rootReducer from './stores/rootReducer'
 import CustomDrawer from './navigation/CustomDrawer'
+import { OnBoarding, SignIn, SignUp, ForgotPassword, Otp } from './screens'
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
 const Stack = createNativeStackNavigator()
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide()
+  }, [])
+
   return (
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{ headerShown: false }}
-          initialRouteName='Home'
+          initialRouteName='OnBoarding'
         >
+          <Stack.Screen name='OnBoarding' component={OnBoarding} />
+          <Stack.Screen name='SignIn' component={SignIn} />
+          <Stack.Screen name='SignUp' component={SignUp} />
+          <Stack.Screen name='ForgotPassword' component={ForgotPassword} />
+          <Stack.Screen name='Otp' component={Otp} />
           <Stack.Screen name='Home' component={CustomDrawer} />
         </Stack.Navigator>
       </NavigationContainer>
