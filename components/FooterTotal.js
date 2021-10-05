99735ee169ab8f10c1ecdfa1ff307edc5457ea0c
andres alcocer <andresalcocer7@yahoo.com>
2021-10-05 10:40:09 -0400

    completed MyCart screen component content

--- components/FooterTotal.js
+++ components/FooterTotal.js
@@ -34,6 +34,35 @@ const FooterTotal = ({ subTotal, shippingFee, total, onPress }) => {
           <Text style={{ flex: 1, ...FONTS.body3 }}>Subtotal</Text>
           <Text style={{ ...FONTS.h3 }}>${subTotal.toFixed(2)}</Text>
         </View>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            marginTop: SIZES.base,
+            marginBottom: SIZES.padding,
+          }}
+        >
+          <Text style={{ flex: 1, ...FONTS.body3 }}>Shipping Fee</Text>
+          <Text style={{ ...FONTS.h3 }}>${shippingFee.toFixed(2)}</Text>
+        </View>
+
+        <LineDivider />
+
+        <View style={{ flexDirection: 'row', marginTop: SIZES.padding }}>
+          <Text style={{ flex: 1, ...FONTS.h2 }}>Total: </Text>
+          <Text style={{ ...FONTS.h2 }}>${total.toFixed(2)}</Text>
+        </View>
+
+        <TextButton
+          buttonContainerStyle={{
+            height: 60,
+            marginTop: SIZES.padding,
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.primary,
+          }}
+          label='Place your Order'
+          onPress={onPress}
+        />
       </View>
     </View>
   )
